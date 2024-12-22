50bd93d3b85850f52ecf86364bbd486aa5ffda3e
zhou274 <274891948@qq.com>
2024-12-22 17:13:13 +0800

    asset

---
+++ UnityParkourGameDemo-main/Assets/Scripts/Logic/GameMgr.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    public static bool isContinue=false;
+    public static int HighScore = 0;
+    private void Awake()
+    {
+        
+        s_instance = this;
+        m_state = GameState.Playing;
+        if(isContinue==false)
+        {
+            PlayerPrefs.SetInt("SCORE", 0);
+            score = 0;
+        }
+        else if(isContinue==true)
+        {
+            score = PlayerPrefs.GetInt("SCORE", score);
+            isContinue = false;
+        }
+    }
+    private void Update()
+    {
+        PlayerPrefs.SetInt("SCORE", score);
+        if(score>PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+    public GameState state
+    {
+        get { return m_state; }
+        set
+        {
+            m_state = value;
+            if(GameState.End == value)
+            {
+                // 游戏结束
+                var canvas = GameObject.Find("Canvas");
+                var prefab = Resources.Load("GameOverPanel");
+                var panel = GameObject.Instantiate(prefab) as GameObject;
+                panel.transform.SetParent(canvas.transform, false);
+            }
+        }
+    }
+
+    private GameState m_state = GameState.Ready;
+    public int score
+    {
+        get { return m_score; }
+        set 
+        {
+            if (value != m_score)
+                EventDispatcher.instance.DispatchEvent(EventNameDef.EVENT_ADD_COIN, value);
+            m_score = value; 
+        }
+    }
+    private int m_score;
+
+
+    private static GameMgr s_instance;
+    public static GameMgr instance { get { return s_instance; } }
+}
+
+public enum GameState
+{
+    Ready,
+    Playing,
+    End,
+}
---
+++ UnityParkourGameDemo-main/Assets/Scripts/UI/GameOverPanel.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverPanel : MonoBehaviour
+{
+    public Button restartBtn;
+    public TextMeshProUGUI HighSocre;
+
+    void Start()
+    {
+        restartBtn.onClick.AddListener(() => 
+        {
+            GameMgr.instance.state = GameState.Playing;
+            SceneManager.LoadScene(1);
+        });
+    }
+    public void Update()
+    {
+        HighSocre.text = PlayerPrefs.GetInt("HighScore").ToString();
+    }
+    public void Continue()
+    {
+        GameMgr.instance.state = GameState.Playing;
+        GameMgr.isContinue = true;
+        SceneManager.LoadScene(1);
+    }
+}
